2a98d93e00bad7821ca777c64e63091ca8877ffa
Gowthamr221 <gowthamr221@gmail.com>
2022-05-11 14:12:36 +0530

    edit urls.py

--- ecommercesite/urls.py
+++ ecommercesite/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/',include('shop.urls')),
     path('blog/',include('blog.urls')),
-] + static(settings.MEDIA_URL,document_root)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
